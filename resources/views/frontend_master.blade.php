24df5fa9bf265ea90de1f7880b31c6a5072fed68
Kyle Phillips <kyle@object9.com>
2017-05-18 11:48:18 -0400

    Master blade template cleanup

--- resources/views/frontend_master.blade.php
+++ resources/views/frontend_master.blade.php
@@ -1,28 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-<!-- Website built by Kyle Phillips – all legislative data provided by the sunlight foundation -->
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Legislator Finder: Find Your Senators &amp; Representatives</title>	
-<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-
-{{Html::style('assets/css/styles.css')}}
-
-<script type="text/javascript" src="//use.typekit.net/nxo4tar.js"></script>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?key={{env('GOOGLE_MAPS_KEY')}}"></script>
-@include('includes.scripts')
-
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Legislator Finder: Find Your Senators &amp; Representatives</title>	
+	<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+	{{Html::style('assets/css/styles.css')}}
+	<script type="text/javascript" src="//use.typekit.net/nxo4tar.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?key={{env('GOOGLE_MAPS_KEY')}}"></script>
+	@include('includes.scripts')
 </head>
-
 <body>
-	
 	@yield('content')
-	
 	<div class="sunlight">Data provided by the <a href="http://sunlightfoundation.com">Sunlight Foundation</a></div>
-
-
 </body>
 </html>
-
