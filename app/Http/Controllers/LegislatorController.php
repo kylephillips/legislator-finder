45915831e7661487f0152c9b21caec415c03ba40
Kyle Phillips <kyle@object9.com>
2017-05-25 16:21:36 -0400

    Propublica test removed

--- app/Http/Controllers/LegislatorController.php
+++ app/Http/Controllers/LegislatorController.php
@@ -170,20 +170,5 @@ class LegislatorController extends Controller
 			->with('center_lat',$center_lat)
 			->with('center_lon',$center_lon)
 			->with('coordinates',$coordinates);
-	}
-
-	
-	public function propublicaTest()
-	{
-		$client = new Client();
-		$pp_feed = "https://api.propublica.org/congress/v1/members/house/GA/4/current.json";
-		$pp_response = $client->get($pp_feed, [
-			'headers' => [
-				'X-API-Key' => env('PROPUBLICA_API_KEY')
-			]
-		]);		
-		$pp_data = $pp_response->json();
-		dd($pp_data);
-	}
-	
+	}	
 }
