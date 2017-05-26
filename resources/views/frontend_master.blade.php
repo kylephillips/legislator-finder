6556cac4f43620e5ce6088983699b16bd9952fce
Kyle Phillips <kyle@object9.com>
2017-05-26 14:04:56 -0400

    Google maps call updated to use https

--- resources/views/frontend_master.blade.php
+++ resources/views/frontend_master.blade.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" href="/assets/css/styles.css?v=1.0">
 	<script type="text/javascript" src="//use.typekit.net/nxo4tar.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-	<script type="text/javascript" src="http://maps.google.com/maps/api/js?key={{env('GOOGLE_MAPS_KEY')}}&libraries=places"></script>
+	<script type="text/javascript" src="https://maps.google.com/maps/api/js?key={{env('GOOGLE_MAPS_KEY')}}&libraries=places"></script>
 	@include('includes.scripts')
 </head>
 <body>
