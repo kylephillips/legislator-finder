94bdf69cee7423178f0f32edd0af6a5b6793d8df
Kyle Phillips <kyle@object9.com>
2017-05-26 12:55:58 -0400

    Scripts moved back to head

--- resources/views/frontend_master.blade.php
+++ resources/views/frontend_master.blade.php
@@ -7,11 +7,11 @@
 	{{Html::style('assets/css/styles.css')}}
 	<script type="text/javascript" src="//use.typekit.net/nxo4tar.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?key={{env('GOOGLE_MAPS_KEY')}}&libraries=places"></script>
+	@include('includes.scripts')
 </head>
 <body>
 	@yield('content')
 	<div class="sunlight">Data provided by <a href="https://developers.google.com/civic-information/">Google</a>, <a href="https://openstates.org/">Open States</a> and <a href="http://census.gov">census.gov</a></div>
 </body>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?key={{env('GOOGLE_MAPS_KEY')}}&libraries=places"></script>
-@include('includes.scripts')
 </html>
