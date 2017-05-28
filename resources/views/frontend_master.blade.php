291563acbffab2cd1a68a813e1ac965a0d16fa12
Kyle Phillips <kylephillipsdesign@gmail.com>
2017-05-28 18:22:28 -0400

    dynamic versioning added

--- resources/views/frontend_master.blade.php
+++ resources/views/frontend_master.blade.php
@@ -4,7 +4,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Legislator Finder: Find Your Senators &amp; Representatives</title>	
 	<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-	<link rel="stylesheet" href="/assets/css/styles.css?v=1.0">
+	<link rel="stylesheet" href="/assets/css/styles.css?v={{env('VERSION_NUMBER')}}">
 	<script type="text/javascript" src="//use.typekit.net/nxo4tar.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	<script type="text/javascript" src="https://maps.google.com/maps/api/js?key={{env('GOOGLE_MAPS_KEY')}}&libraries=places"></script>
